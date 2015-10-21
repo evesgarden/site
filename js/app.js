94e23190e1d4dd3dd3e39a41c404c80aae440698
ujjwal <ujjwal.narayan@gmail.com>
2015-10-20 22:14:13 -0500

    latest

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 
    var app = angular.module("eve", ["ui.bootstrap", "ngSanitize"]);
    
-   var blogger_api_key = "AIzaSyDeYPnn4gOcfitqdIWHMBR6oWuT6EOv2zQ";
-   var RestBaseUrl = "https://www.googleapis.com/blogger/v3/blogs/7068809791937788032";
+   var blogger_api_key = "AIzaSyCkEcxsMt3bfI9JJyN_hoLznEnYakCOR64"; // "AIzaSyDeYPnn4gOcfitqdIWHMBR6oWuT6EOv2zQ";
+   var RestBaseUrl = "https://www.googleapis.com/blogger/v3/blogs/4636194870453825564"; //7068809791937788032";
    
    app.controller("FeedCtrl", ["$scope", "$http", function($scope, $http) {
     
